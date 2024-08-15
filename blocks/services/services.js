7c0afdf8a1f0b54e37f72216b1ff9a3901f7063c
Cole Walker <colew@ensemble.com>
2024-08-15 12:36:12 -0700

    fix lint errors

--- blocks/services/services.js
+++ blocks/services/services.js
@@ -1,10 +1,10 @@
-export default function decorate(block) { 
+export default function decorate(block) {
   // Create a container for the service cards
   const cardContainer = document.createElement('div');
   cardContainer.classList.add('card-container');
 
   // Iterate over each child element within the block
-  [...block.children].forEach((child) => {
+  [...block.children].forEach(child => {
     // Add a class to each child element for clearer CSS targeting
     child.classList.add('card');
 
@@ -16,7 +16,7 @@ export default function decorate(block) {
     const infoWrapper = child.lastElementChild;
     infoWrapper.classList.add('info-wrapper');
 
-    // Find and remove the last <p> element that contains the link. Keep track of href value. 
+    // Find and remove the last <p> element that contains the link. Keep track of href value.
     const lastParagraph = infoWrapper.querySelector('p:last-of-type');
     const link = lastParagraph.querySelector('a');
     let href = '#';
@@ -25,7 +25,7 @@ export default function decorate(block) {
       href = link?.href;
       lastParagraph.remove();
     }
-  
+
     // Create a new <a> element to wrap the entire card, enabling redirect on click
     const wrapperLink = document.createElement('a');
     wrapperLink.href = href;
